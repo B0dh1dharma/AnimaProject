e9ecccd084fc98d1376e38ba52fe123bcf91c303
B0dh1dharma <alrik_ranulfsson@hotmail.com>
2014-11-07 04:18:45 +0100

    Jugando con Prueba2 para juntar un bloque y dejar sitio para la imagen

--- Anima/src/GUI/Prueba2.java
+++ Anima/src/GUI/Prueba2.java
@@ -12,24 +12,17 @@ public class Prueba2 extends JFrame{
 		setSize(500,800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		GridLayout grid=new GridLayout(4,2);
+		GridLayout grid=new GridLayout(1,2);
 		setLayout(grid);
 		
 		JPanel bloque1=new JPanel();
-		//GridLayout layout1=new GridLayout(1,2,10,10);
-		FlowLayout layout1=new FlowLayout(FlowLayout.CENTER);
+		GridLayout layout1=new GridLayout(2,2,10,10);
+		//FlowLayout layout1=new FlowLayout(FlowLayout.CENTER);
 		bloque1.setLayout(layout1);
 		JLabel nombre=new JLabel("Nombre",JLabel.RIGHT);
 		JTextField campoNombre= new JTextField(10);
 		bloque1.add(nombre);
 		bloque1.add(campoNombre);
-		add(bloque1);
-		
-		JPanel bloque2=new JPanel();
-		//GridLayout layout2=new GridLayout(1,2,10,10);
-		FlowLayout layout2=new FlowLayout(FlowLayout.CENTER);
-		//bloque2.setLayout(layout2);
-		bloque2.setLayout(layout1);
 		JLabel nivel=new JLabel("Nivel",JLabel.RIGHT);
 		JComboBox campoNivel= new JComboBox();
 		campoNivel.addItem(1);
@@ -37,8 +30,19 @@ public class Prueba2 extends JFrame{
 		campoNivel.addItem(3);
 		campoNivel.addItem(4);
 		campoNivel.addItem(5);
-		bloque2.add(nivel);
-		bloque2.add(campoNivel);
+		bloque1.add(nivel);
+		bloque1.add(campoNivel);
+		add(bloque1);
+		
+		//Panel vacio para probar huecos
+		
+		JPanel bloque2=new JPanel();
+		//GridLayout layout2=new GridLayout(1,2,10,10);
+		FlowLayout layout2=new FlowLayout(FlowLayout.CENTER);
+		//bloque2.setLayout(layout2);
+		bloque2.setLayout(layout1);
+		JLabel prueba=new JLabel("PRUEBA",JLabel.CENTER);
+		bloque2.add(prueba);
 		add(bloque2);
 		
 		setVisible(true);
@@ -48,7 +52,7 @@ public class Prueba2 extends JFrame{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		PruebaBase frame=new PruebaBase();
+		Prueba2 frame=new Prueba2();
 	}
 
 }
